4827cd412a3dd771499143419457981b9d7ff50c
leozizz <leonardocps2015@gmail.com>
2022-10-28 15:45:11 -0300

    Implementando funções

--- salario.js
+++ salario.js
@@ -1,20 +1,20 @@
 import dados from 'readline-sync'
 
-let salario = [
-    {ano: 2010, salario: 510},
-    {ano: 2011, salario: 545},
-    {ano: 2012, salario: 622},
-    {ano: 2013, salario: 678},
-    {ano: 2014, salario: 724},
-    {ano: 2015, salario: 788},
-    {ano: 2016, salario: 880},
-    {ano: 2017, salario: 937},
-    {ano: 2018, salario: 954},
-    {ano: 2019, salario: 998},
-    {ano: 2020, salario: 1045}
+let listaSalario = [
+    {ano: 2010, salario: 510.00},
+    {ano: 2011, salario: 545.00},
+    {ano: 2012, salario: 622.00},
+    {ano: 2013, salario: 678.00},
+    {ano: 2014, salario: 724.00},
+    {ano: 2015, salario: 788.00},
+    {ano: 2016, salario: 880.00},
+    {ano: 2017, salario: 937.00},
+    {ano: 2018, salario: 954.00},
+    {ano: 2019, salario: 998.00},
+    {ano: 2020, salario: 1045.00}
 ]
 
-let inflacao = [
+let listaInflacao = [
     {ano: 2010, inflacao: 5.91},
     {ano: 2011, inflacao: 6.50},
     {ano: 2012, inflacao: 5.84},
@@ -28,8 +28,45 @@ let inflacao = [
     {ano: 2020, inflacao: 4.52}
 ]
 
-let opcao = dados.question(`Escolha uma das alternativas:\n\n
-                            1 - Listar os salários minímos de 2010 à 2020
-                            2 - Listar o índice IPCA de 2010 à 2020
-                            3 - Comparação entre o percentual de aumento salarial e o IPCA\n\n
-                            Digite o número de sua escolha: `)
+let label_ano = "Ano: "
+let label_salario = "Salario mínimo: "
+let label_inflacao = "Inflação IPCA: "
+
+let opcao = dados.question(
+    `Escolha uma das alternativas:
+
+    1 - Listar os salários minímos de 2010 à 2020
+    2 - Listar o índice IPCA de 2010 à 2020
+    3 - Comparação entre o percentual de aumento salarial e o IPCA
+
+    Digite o número de sua escolha: `)
+
+switch (opcao) {
+    case '1':
+        mostraSalario()
+        break;
+    
+    case '2':
+        mostraInflacao()
+        break;
+
+    case '3':
+        break;
+
+    default:
+        break;
+}
+
+function mostraSalario () {
+    for (let salario of listaSalario) {
+        console.log(`${label_ano.padEnd(30, ".")} ${salario.ano}`)
+        console.log(`${label_salario.padEnd(30, ".")} ${salario.salario}\n`)
+    }
+}
+
+function mostraInflacao () {
+    for (let inflacao of listaInflacao) {
+        console.log(`${label_ano.padEnd(30, ".")} ${inflacao.ano}`)
+        console.log(`${label_inflacao.padEnd(30, ".")} ${inflacao.inflacao}`)
+    }
+}
